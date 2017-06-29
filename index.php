d0b9e3d4e39a623e630649543c166643d2c9ec49
Bennet Gallein <bennet@intranetproject.net>
2017-06-29 21:18:02 +0200

    main, docs, blog, shop, json

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+function is_url_exist($url){
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_NOBODY, true);
+    curl_exec($ch);
+    $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    if($code == 200){
+        $status = '<p class="online st">up!</p>';
+    }else{
+        $status = '<p class="offline st">down!</p>';
+    }
+    curl_close($ch);
+    return $status;
+}
+?>
+<html>
+<head>
+    <title>IntranetProject - Status</title>
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <h1 class="title"><b>IntranetProject Status</b></h1>
+    <div class="content">
+        <div class="inner">
+            <small class="notice">
+                <p>Click on any Name to visit the site!</p>
+            </small>
+            <div class="clearer"></div>
+            <a target="_blank" class="site" href="http://intranetproject.net">Main site is </a>
+            <?php
+            echo is_url_exist("http://intranetproject.net/up.txt");
+            ?>
+            <div class="clearer"></div>
+            <a target="_blank" class="site" href="http://docs.intranetproject.net">Docs are </a>
+            <?php
+            echo is_url_exist("http://docs.intranetproject.net/up.txt");
+            ?>
+            <div class="clearer"></div>
+            <a target="_blank" class="site" href="http://blog.intranetproject.net">Blog is </a>
+            <?php
+            echo is_url_exist("http://blog.intranetproject.net/up.txt");
+            ?>
+            <div class="clearer"></div>
+            <a target="_blank" class="site" href="http://shop.intranetproject.net">Shop is </a>
+            <?php
+            echo is_url_exist("http://shop.intranetproject.net/up.txt");
+            ?>
+            <div class="clearer"></div>
+            <a target="_blank" class="site" href="http://json.intranetproject.net">Json Tool is </a>
+            <?php
+            echo is_url_exist("http://json.intranetproject.net/up.txt");
+            ?>
+            <div class="clearer"></div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
