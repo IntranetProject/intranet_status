a46768c2384ca2609842f941173f40eaf45a0d14
TobiasK25 <tobika2000.tk@gmail.com>
2017-07-17 20:36:30 +0200

    footer added

--- index.php
+++ index.php
@@ -14,52 +14,66 @@ function is_url_exist($url){
     return $status;
 }
 ?>
-<html>
-<head>
-    <title>IntranetProject - Status</title>
-    <link href="css/reset.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <html>
 
-</head>
-<body>
-<div class="container">
-    <h1 class="title"><b>IntranetProject Status</b></h1>
-    <div class="content">
-        <div class="inner">
-            <small class="notice">
+    <head>
+        <title>IntranetProject - Status</title>
+        <link href="css/reset.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+
+    </head>
+
+    <body>
+        <div class="container">
+            <h1 class="title"><b>IntranetProject Status</b></h1>
+            <div class="content">
+                <div class="inner">
+                    <small class="notice">
                 <p>Click on any Name to visit the site!</p>
             </small>
-            <div class="clearer"></div>
-            <a target="_blank" class="site" href="http://intranetproject.net">Main site is </a>
-            <?php
+                    <div class="clearer"></div>
+                    <a target="_blank" class="site" href="http://intranetproject.net">Main site is </a>
+                    <?php
             echo is_url_exist("http://intranetproject.net/up.txt");
             ?>
-            <div class="clearer"></div>
-            <a target="_blank" class="site" href="http://docs.intranetproject.net">Docs are </a>
-            <?php
+                        <div class="clearer"></div>
+                        <a target="_blank" class="site" href="http://docs.intranetproject.net">Docs are </a>
+                        <?php
             echo is_url_exist("http://docs.intranetproject.net/up.txt");
             ?>
-            <div class="clearer"></div>
-            <a target="_blank" class="site" href="http://blog.intranetproject.net">Blog is </a>
-            <?php
+                            <div class="clearer"></div>
+                            <a target="_blank" class="site" href="http://blog.intranetproject.net">Blog is </a>
+                            <?php
             echo is_url_exist("http://blog.intranetproject.net/up.txt");
             ?>
-            <div class="clearer"></div>
-            <a target="_blank" class="site" href="http://shop.intranetproject.net">Shop is </a>
-            <?php
+                                <div class="clearer"></div>
+                                <a target="_blank" class="site" href="http://shop.intranetproject.net">Shop is </a>
+                                <?php
             echo is_url_exist("http://shop.intranetproject.net/up.txt");
             ?>
-            <div class="clearer"></div>
-            <a target="_blank" class="site" href="http://json.intranetproject.net">JSON tool is </a>
-            <?php
+                                    <div class="clearer"></div>
+                                    <a target="_blank" class="site" href="http://json.intranetproject.net">JSON tool is </a>
+                                    <?php
             echo is_url_exist("http://json.intranetproject.net/up.txt");
             ?>
-            <div class="clearer"></div>
+                                        <div class="clearer"></div>
+                </div>
+            </div>
+            <div class="discord">
+                <iframe src="https://discordapp.com/widget?id=313774393342033920&theme=dark" height="500" allowtransparency="true" frameborder="0"></iframe>
+            </div>
+            <div class="footer">
+                <div class="git">
+                    <p><a href="https://github.com/IntranetProject">GitHub</a></p>
+                </div>
+                <div class="imp">
+                    <p><a href="imprint.html">Impressum</a></p>
+                </div>
+                <div class="credit">
+                    <p><b><i style="color: black;">&lt; &gt;</i></b> with <i style="color: red;">&hearts;</i> by <a href="https://github.com/IntranetProject">Intranet</a></p>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="discord">
-        <iframe src="https://discordapp.com/widget?id=313774393342033920&theme=dark" height="500" allowtransparency="true" frameborder="0"></iframe>
-    </div>
-</div>
-</body>
-</html>
+    </body>
+
+    </html>
